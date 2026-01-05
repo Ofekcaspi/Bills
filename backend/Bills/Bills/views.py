850a8f0c9968e95283adf3aab35c85250448aca7
omri_arg <r3sided@gmail.com>
2026-01-05 17:48:35 +0200

    fix

--- backend/Bills/Bills/views.py
+++ backend/Bills/Bills/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 def hello_view(request):
     return HttpResponse("hello")
-
+@api_view(['GET'])
 def get_emails(request):
     return HttpResponse("hello")
