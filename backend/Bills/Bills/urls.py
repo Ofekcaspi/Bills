f8f157c6351ace568e9aa0bbfb25e4ba3ad1c77b
omri_arg <r3sided@gmail.com>
2026-01-05 17:44:05 +0200

    getemails

--- backend/Bills/Bills/urls.py
+++ backend/Bills/Bills/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from views import get_emails
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('get-emails/', get_emails),
 ]
--- backend/Bills/Bills/views.py
+++ backend/Bills/Bills/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello_view(request):
     return HttpResponse("hello")
+
+def get_emails(request):
+    return HttpResponse("hello")
