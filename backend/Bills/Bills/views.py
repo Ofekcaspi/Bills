d7d13d740b21e4b99e67f0ce2a92b28f1a8d0f0a
omri_arg <r3sided@gmail.com>
2026-01-12 15:05:46 +0200

    addgetemails

--- backend/Bills/Bills/views.py
+++ backend/Bills/Bills/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from .gmailConnect import GmailAuthService
+from .mailConnect import GmailAuthService
 
 
 def _auth_service() -> GmailAuthService:
